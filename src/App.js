2bfaf6b98ac37902eb31fe441d509a1568e6411b
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-05-01 13:00:43 +0545

    including images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import ProfileCard from "./ProfileCard";
 
-function App() {
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
+console.log(AlexaImage);
+console.log(SiriImage);
+
+function App() {
     return (
         <div>
             <div>Personal Digital Assistants</div>
+
+            <img src={AlexaImage} />
+            <img src={SiriImage} />
 
             <ProfileCard title="Alexa" handle="@alexa99" />
             <ProfileCard title="Cortana" handle="@cortana32" />
