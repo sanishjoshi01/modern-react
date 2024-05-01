550bcfd3fa3f4a32451318c8f1fed1325d6ba211
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-05-01 13:10:04 +0545

    handling image accessibility

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ import AlexaImage from "./images/alexa.png";
 import CortanaImage from "./images/cortana.png";
 import SiriImage from "./images/siri.png";
 
-console.log(AlexaImage);
-console.log(SiriImage);
-
 function App() {
     return (
         <div>
             <div>Personal Digital Assistants</div>
 
-            <img src={AlexaImage} />
-            <img src={SiriImage} />
-
-            <ProfileCard title="Alexa" handle="@alexa99" />
-            <ProfileCard title="Cortana" handle="@cortana32" />
-            <ProfileCard title="Siri" handle="@siri01" />
+            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+            <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+            <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
         </div>
     );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,6 +1,7 @@
-const ProfileCard = ({ title, handle }) => {
+const ProfileCard = ({ title, handle, image }) => {
     return (
         <div>
+            <img src={image} alt="PDA logo" />
             <div>Title is {title}</div>
             <div>Username is {handle}</div>
         </div>
