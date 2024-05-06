73574a800c3b5a989eb37edf339e8eeed6b06f0e
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-05-06 12:45:46 +0545

    addding book object

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ function App() {
     const [books, setBooks] = useState([]);
 
     const createBook = (title) => {
-        console.log("Create a book with title: ", title)
+        const updatedBooks = [
+            ...books,
+            { id: 123, title: title }
+        ]
+
+        setBooks(updatedBooks);
     }
 
     return (
         <div>
+            {books.length}
             <BookCreate onCreate={createBook} />
         </div>
     )
