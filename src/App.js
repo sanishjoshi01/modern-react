6da17cc0b5c1574727ee025d199b2b34d58b36ea
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-05-10 14:01:53 +0545

    Updating Book

--- src/App.js
+++ src/App.js
@@ -16,10 +16,14 @@ function App() {
         fetchBooks()
     }, []);
 
-    const editBookById = (id, newTitle) => {
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put('http://localhost:3001/books/' + id, {
+            title: newTitle
+        });
+
         const updatedBooks = books.map((book) => {
             if (book.id === id) {
-                return { ...book, title: newTitle }
+                return { ...book, ...response.data }
             }
 
             return book;
