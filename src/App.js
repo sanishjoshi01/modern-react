0a56cf69e64f3acd79488f37689127b150bdf2e0
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-05-10 13:28:06 +0545

    fetching list of books and useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 import axios from "axios";
 
 function App() {
     const [books, setBooks] = useState([]);
+
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+
+        setBooks(response.data);
+    }
+
+    useEffect(() => {
+        fetchBooks()
+    }, []);
 
     const editBookById = (id, newTitle) => {
         const updatedBooks = books.map((book) => {
