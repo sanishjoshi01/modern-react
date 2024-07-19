fc2e5f664ee0d697b6926fd061f7a3a21597e7af
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-07-19 15:01:56 +0545

    async thunk functions rtk

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,5 +1,27 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../store";
+
 function UsersList() {
-    return "users lsit";
+    const dispatch = useDispatch();
+    const { data, isLoading, error } = useSelector((state) => {
+        return state.users;
+    });
+
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, []);
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+    if (error) {
+        return <div>Error Fetching Data</div>
+    }
+
+    return (
+        <div>{data.length}</div>
+    );
 }
 
 export default UsersList;
--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,12 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUsers } from "../index";
 
 const usersSlice = createSlice({
     name: 'users',
     initialState: {
         data: [],
+        isLoading: false,
+        error: null,
     },
-    reducers: {
-
+    extraReducers(builder) {
+        builder.addCase(fetchUsers.pending, (state, action) => {
+            state.isLoading = true;
+        });
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.data = action.payload; //payload automatically contains data from thunk function fetchUsers
+        });
+        builder.addCase(fetchUsers.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.error; //error is automatically added to action by thunk functin
+        });
     }
 });
 
